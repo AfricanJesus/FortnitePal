eef232604a1b42a30071ea8cf8a47a2222697f80
Tyree <tyreegustafson@live.com>
2018-08-30 04:12:21 -0500

    base project

---
+++ src/main/java/com/africanjesus/fortnitepal/FortnitepalApplication.java
@@ -0,0 +1,12 @@
+package com.africanjesus.fortnitepal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortnitepalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortnitepalApplication.class, args);
+	}
+}
---
+++ src/main/java/com/africanjesus/fortnitepal/RarityTypes.java
@@ -0,0 +1,5 @@
+package com.africanjesus.fortnitepal;
+
+public enum RarityTypes {
+    LEGENDARY, EPIC, RARE, COMMON;
+}
