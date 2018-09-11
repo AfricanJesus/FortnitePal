f04ce820fb1a18337dece1b2fde9584c4d41e565
Tyree <tyreegustafson@live.com>
2018-09-11 00:55:30 -0500

    Implement unreleased items component

--- src/main/java/com/africanjesus/fortnitepal/repositories/ItemRepository.java
+++ src/main/java/com/africanjesus/fortnitepal/repositories/ItemRepository.java
@@ -2,6 +2,7 @@ package com.africanjesus.fortnitepal.repositories;
 
 import com.africanjesus.fortnitepal.ItemType;
 import com.africanjesus.fortnitepal.RarityType;
+import com.africanjesus.fortnitepal.ReleaseStatus;
 import com.africanjesus.fortnitepal.model.Item;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -19,4 +20,5 @@ public interface ItemRepository extends JpaRepository<Item, Long>{
     List<Item> findByRarityType(@Param("rarity") RarityType rarity, Pageable p);
     List<Item> findByItemType(@Param("item") ItemType item, Pageable p);
     List<Item> findByObtained_Season(@Param("season")int season, Pageable p);
+    List<Item> findByStatus(@Param("status")ReleaseStatus status, Pageable p);
 }
